96effe538a4d3e1ec8ecc86b5ea937bf566fc909
video <video>
2017-04-18 12:53:48 +0100

    fix: using eval for env variables subs. LC

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 
 echo "Starting disk-cleanup..."
 
-sed -i -e "s/\${cron_cleanup_hours}/${cron_cleanup_hours}/" -e "s/\${cron_cleanup_hourstodelete}/${cron_cleanup_hourstodelete}/" /var/spool/cron/crontabs/root
+eval "cat <<EOF
+$(</var/spool/cron/crontabs/root)
+EOF" > /var/spool/cron/crontabs/root
 
 crond -l 0 -f -L /var/sky/log/disk-cleanup/cron.log
